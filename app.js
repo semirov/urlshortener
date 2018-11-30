9f7a0f2877ad766edd24ae11f9deb2236ce081f7
Filipp <semirov@norvix.ru>
2018-11-30 15:12:07 +0400

    generate express project

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'client')));
+
+app.use('/', indexRouter);
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+  });
+
+module.exports = app;
