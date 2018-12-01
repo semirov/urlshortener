f4d599b345741d322c4123ca362e668484680532
EC2 Default User <ec2-user@ip-172-31-32-184.us-east-2.compute.internal>
2018-12-01 10:31:14 +0000

    add api routers and send angular in another path

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require("mongoose");
+var body-parser = require("body-parser");
+
+var apiRouter = require('./routes/apiRouter');
 var config = require("./config");
-
-
-
-var indexRouter = require('./routes/index');
 
 var app = express();
 
@@ -15,14 +14,18 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'client/dist/client')));
 
-app.use('/', indexRouter);
+app.use('/api', apiRouter);
+app.use(express.static(path.join(__dirname, 'client/dist/client')));
+app.all('/*', (req, res) => {res.status(200).sendFile(path.join(__dirname, 'client/dist/client/index.html'));
+        });
+
 
 mongoose.connect(config.db.path(), { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'database connection error!'));
 db.on('connected', console.log.bind(console, 'app connected to database!'));
+
 
 app.listen(config.app.port, function () {
     console.log(`Urlshortener listening on port: ${config.app.port}`);
