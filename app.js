0e2af51826b2bd35c0b184e53b8135ea4a39d98b
EC2 Default User <ec2-user@ip-172-31-32-184.us-east-2.compute.internal>
2018-12-01 09:25:33 +0000

    generate latest anguar client

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'client')));
+app.use(express.static(path.join(__dirname, 'client/dist/client')));
 
 app.use('/', indexRouter);
 
